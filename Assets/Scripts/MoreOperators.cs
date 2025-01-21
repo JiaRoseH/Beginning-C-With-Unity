c722c00400f0c19fd977ec47a5444af5c144bf97
Jia Holt <jiaroseh@gmail.com>
2025-01-20 20:20:18 -0500

    Video 8
    Logical Operators

--- Assets/Scripts/MoreOperators.cs
+++ Assets/Scripts/MoreOperators.cs
@@ -5,23 +5,37 @@ using UnityEngine;
 public class MoreOperators : MonoBehaviour
 {
 
-    void Start()
+    void Start() // to comment all out, ctrl + /
     {
-        int x = 1;
-        int y = 2;
-        //bool result = x == y;      // outputs false, obviously
-        //bool result = x != y;      // outputs true, obviously
+        // int x = 1;
+        // int y = 2;
+        // //bool result = x == y;      // outputs false, obviously
+        // //bool result = x != y;      // outputs true, obviously
 
-        string playerName = null;
-        //bool result = playerName != null;         // false
+        // string playerName = null;
+        // //bool result = playerName != null;         // false
 
-        int nextLevelXP = 300;                      // false
-        int playerOneXP = 185;
-        bool result = playerOneXP > nextLevelXP;    // false
+        // int nextLevelXP = 300;                      // false
+        // int playerOneXP = 185;
+        // bool result = playerOneXP > nextLevelXP;    // false
 
-        Debug.Log(!result);          // does not equal result baked in
-        Debug.Log(result);
-        
+        // Debug.Log(!result);          // does not equal result baked in
+        // Debug.Log(result);
+
+        int coins = 100;
+        int totalLives = 6;
+        int maxLives = 10;
+        bool isPremiumMember = false;
+
+        bool shouldIncreaseDifficulty = totalLives >= 5 && !isPremiumMember;
+        Debug.Log(shouldIncreaseDifficulty);
+
+        bool shouldIncreaseLifeCount = coins >= 100 && totalLives < maxLives || isPremiumMember;
+        Debug.Log(shouldIncreaseLifeCount);
+
+        string firstName = null;
+        bool isUpperCase = firstName != null && firstName.ToUpper() == "JIA";     // evaluates to false because the first case is false, skips to next line
+        Debug.Log(isUpperCase);
     }
 
     void Update()
